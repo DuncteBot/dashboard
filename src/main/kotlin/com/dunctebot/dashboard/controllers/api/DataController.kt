48cd7d4aff4a2ed307ce1fa60c13a16b10e4328e
duncte123 <contact@duncte123.me>
2020-11-27 15:31:27 +0100

    HotFix: database not sending proper data updates

--- src/main/kotlin/com/dunctebot/dashboard/controllers/api/DataController.kt
+++ src/main/kotlin/com/dunctebot/dashboard/controllers/api/DataController.kt
@@ -28,6 +28,7 @@ import com.dunctebot.dashboard.authOrFail
 import com.dunctebot.dashboard.duncteApis
 import com.dunctebot.dashboard.jsonMapper
 import com.dunctebot.dashboard.webSocket
+import com.fasterxml.jackson.databind.node.ObjectNode
 import spark.Request
 
 object DataController {
@@ -36,9 +37,12 @@ object DataController {
 
         // parse the data to make sure that it is proper json
         val updateData = jsonMapper.readTree(request.bodyAsBytes())
+        val request = jsonMapper.createObjectNode()
+            .put("t", "UPDATE_DATA")
+            .set<ObjectNode>("d", updateData)
 
         // send the data to all instances that are connected
-        webSocket.broadcast(updateData)
+        webSocket.broadcast(request)
 
         return jsonMapper.createObjectNode().put("success", true)
     }
