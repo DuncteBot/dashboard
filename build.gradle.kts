e5e5bf7c31af8bdfc6eca5d5a0818bde74420352
duncte123 <contact@duncte123.me>
2021-03-03 09:52:19 +0100

    Update invite domain on dashboard

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (c) 2020 Duncan Sterken
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 plugins {
 //    java
     application
@@ -33,10 +9,11 @@ group = "com.dunctebot"
 version = "1.0"
 
 repositories {
+    mavenCentral()
     jcenter()
 
     maven {
-        url = uri("https://dl.bintray.com/duncte123/maven")
+        url = uri("https://duncte123.jfrog.io/artifactory/maven")
     }
 
     maven {
@@ -46,7 +23,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(group = "com.dunctebot", name = "dunctebot-models", version = "0.0.12")
+    implementation(group = "com.dunctebot", name = "dunctebot-models", version = "0.1.20")
 
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
 
