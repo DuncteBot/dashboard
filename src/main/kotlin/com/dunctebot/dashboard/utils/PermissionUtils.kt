eb75697091124a4c2605d50d19b170472cab43db
duncte123 <contact@duncte123.me>
2021-07-19 10:06:38 +0200

    Create loads more methods

--- src/main/kotlin/com/dunctebot/dashboard/utils/PermissionUtils.kt
+++ src/main/kotlin/com/dunctebot/dashboard/utils/PermissionUtils.kt
@@ -1,13 +1,18 @@
 package com.dunctebot.dashboard.utils
 
 import discord4j.common.util.Snowflake
+import discord4j.core.`object`.ExtendedPermissionOverwrite
 import discord4j.core.util.PermissionUtil
+import discord4j.discordjson.json.ChannelData
 import discord4j.discordjson.json.MemberData
 import discord4j.discordjson.json.RoleData
 import discord4j.rest.entity.RestGuild
 import discord4j.rest.util.PermissionSet
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.*
 import java.util.function.Predicate
+import java.util.stream.Collectors
 
 fun RestGuild.getEveryoneRole(): Mono<RoleData> {
     return this.role(this.id).data
@@ -19,15 +24,37 @@ fun RestGuild.getEveryoneRole(): Mono<RoleData> {
 fun MemberData.getBasePermissions(guild: RestGuild): Mono<PermissionSet> {
     val getIsOwner = guild.data.map { it.ownerId() == this.user().id() }
     val getEveryonePerms = guild.getEveryoneRole().map { PermissionSet.of(it.permissions()) }
-    val getRolePerms = Mono.create<List<PermissionSet>> {
-        this.roles().map { guild.role(Snowflake.of(it)).data }
-            .map {
-                it.flatMap { r -> Mono.from<PermissionSet> { PermissionSet.of(r.permissions()) } }
-            }
-            .flatMap { it.block()!! }
-    }
+    val getRolePerms = Flux.fromIterable(this.roles())
+        .flatMap { guild.role(Snowflake.of(it)).data }
+        .map { PermissionSet.of(it.permissions()) }
+        .collectList()
 
     return getIsOwner.filter(Predicate.isEqual(true))
-        .flatMap { ignored -> Mono.just(PermissionSet.all()) }
+        .flatMap { Mono.just(PermissionSet.all()) }
         .switchIfEmpty(Mono.zip(getEveryonePerms, getRolePerms, PermissionUtil::computeBasePermissions))
+}
+
+fun ChannelData.getPermissionOverwrites(): Set<ExtendedPermissionOverwrite> {
+    return this.permissionOverwrites().toOptional()
+        .map { permissionOverwrites ->
+            val guildId = guildId().get().asLong()
+            val channelId = id().asLong()
+
+            return@map permissionOverwrites.stream()
+                .map { data ->
+                    // TODO: how to turn this into rest
+                    ExtendedPermissionOverwrite(getClient(), data, guildId, channelId)
+                }
+                .collect(Collectors.toSet())
+        }
+        .orElse(Collections.emptySet())
+}
+
+fun ChannelData.getEffectivePermissions(guild: RestGuild, member: MemberData): Mono<PermissionSet> {
+    return member.getBasePermissions(guild).map { basePerms ->
+
+        this.permissionOverwrites().toOptional()
+
+        return@map PermissionUtil.computePermissions(basePerms, null, listOf(), null)
+    }
 }
