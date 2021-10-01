fef30a03e0de43697067dd48af9cbdf6b90c1f17
duncte123 <contact@duncte123.me>
2021-10-01 10:07:36 +0200

    Play around a bit

--- src/main/resources/vue/views/test.vue
+++ src/main/resources/vue/views/test.vue
@@ -1,8 +1,19 @@
 <template id="thread-view">
-    <div>{{ $javalin.pathParams["user"] }}</div>
+    <div>
+        <p>{{ $javalin.pathParams["user"] }}</p>
+        <section>
+            <input type="text" v-model="user">
+            <a :href="`/vue/${user}`">Update</a>
+        </section>
+    </div>
 </template>
 <script>
     Vue.component("thread-view", { // the component name is what we need to enter in VueComponent("...")
-        template: "#thread-view"
+        template: "#thread-view",
+        data () {
+            return {
+                user: this.$javalin.pathParams["user"],
+            };
+        },
     });
 </script>
